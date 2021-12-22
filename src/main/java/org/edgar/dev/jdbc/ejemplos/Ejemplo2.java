bee2e681325afb0947bdb17c158b3586b8d72d1f
edhymx1 <edhy.mxm@gmail.com>
2021-12-22 09:23:57 -0600

    2

--- src/main/java/org/edgar/dev/jdbc/ejemplos/Ejemplo2.java
+++ src/main/java/org/edgar/dev/jdbc/ejemplos/Ejemplo2.java
@@ -9,35 +9,36 @@ public class Ejemplo2 extends Base {
     Repositorio<Producto> repositorio = new ProductoRepositorioImpl();
 
     public void listar() {
-        getLogger().info("Listando productos");
+        getLogger().info("==== Listando productos ====");
         repositorio.listar().forEach(p -> getLogger().info("{}", p));
-        getLogger().info("\n\n\n");
+        getLogger().info("==== Fin Listando productos ====\n\n");
     }
 
     public void buscarPorId(Long id) {
-        getLogger().info("Buscando producto");
+        getLogger().info("==== Buscando producto ====");
+        getLogger().info("id: {}", id);
         getLogger().info("{}", repositorio.buscarPorId(id));
-        getLogger().info("\n\n\n");
+        getLogger().info("==== FIn Buscando producto ====\n\n");
     }
 
     public void crear(Producto producto) {
-        getLogger().info("Guardando producto");
+        getLogger().info("==== Guardando producto ====");
+        getLogger().info("{}", producto);
         repositorio.guardar(producto);
-        getLogger().info("Producto guardado correctamente");
-        getLogger().info("\n\n\n");
+        getLogger().info("==== Fin Guardando producto ====\n\n");
     }
 
     public void actualizar(Producto producto) {
-        getLogger().info("Actualizando producto");
+        getLogger().info("==== Actualizando producto ====");
+        getLogger().info("{}", producto);
         repositorio.guardar(producto);
-        getLogger().info("Producto actualizado correctamente");
-        getLogger().info("\n\n\n");
+        getLogger().info("==== Fin Actualizando producto ====\n\n");
     }
 
     public void eliminar(Long id) {
-        getLogger().info("Eliminando producto");
+        getLogger().info("==== Eliminando producto ====");
+        getLogger().info("id: {}", id);
         repositorio.eliminar(id);
-        getLogger().info("Producto eliminado");
-        getLogger().info("\n\n\n");
+        getLogger().info("==== Fin Eliminando producto ====\n\n");
     }
 }
