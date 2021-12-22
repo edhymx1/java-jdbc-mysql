cd1e2878c1ca4e31ff03fa2912c8705370ab23e2
edhymx1 <edhy.mxm@gmail.com>
2021-12-21 22:29:10 -0600

    Fixes

--- src/main/java/org/edgar/dev/jdbc/EjemploJdbc.java
+++ src/main/java/org/edgar/dev/jdbc/EjemploJdbc.java
@@ -9,13 +9,12 @@ class EjemploJdbc {
     public static void main(String[] args) {
         Ejemplo2 ejemplo2 = new Ejemplo2();
         ejemplo2.listar();
-        ejemplo2.buscarPorId(1l);
-        ejemplo2.crear(new Producto(null, "Poco X3 PRO", 5600, new Date()));
+        ejemplo2.buscarPorId(3l);
+        ejemplo2.crear(new Producto(null, "Poco X3 PRO MAX", 8900, new Date()));
         ejemplo2.listar();
-        ejemplo2.actualizar(new Producto(1l, "J7 MAX", 3000, new Date()));
+        ejemplo2.actualizar(new Producto(3l, "Y9 LITE", 6500, new Date()));
         ejemplo2.listar();
-        ejemplo2.eliminar(1l);
+        ejemplo2.eliminar(3l);
         ejemplo2.listar();
-
     }
 }
--- src/main/java/org/edgar/dev/jdbc/ejemplos/Ejemplo2.java
+++ src/main/java/org/edgar/dev/jdbc/ejemplos/Ejemplo2.java
@@ -9,28 +9,35 @@ public class Ejemplo2 extends Base {
     Repositorio<Producto> repositorio = new ProductoRepositorioImpl();
 
     public void listar() {
+        getLogger().info("Listando productos");
         repositorio.listar().forEach(p -> getLogger().info("{}", p));
+        getLogger().info("\n\n\n");
     }
 
     public void buscarPorId(Long id) {
+        getLogger().info("Buscando producto");
         getLogger().info("{}", repositorio.buscarPorId(id));
+        getLogger().info("\n\n\n");
     }
 
     public void crear(Producto producto) {
         getLogger().info("Guardando producto");
         repositorio.guardar(producto);
         getLogger().info("Producto guardado correctamente");
+        getLogger().info("\n\n\n");
     }
 
     public void actualizar(Producto producto) {
         getLogger().info("Actualizando producto");
         repositorio.guardar(producto);
         getLogger().info("Producto actualizado correctamente");
+        getLogger().info("\n\n\n");
     }
 
     public void eliminar(Long id) {
         getLogger().info("Eliminando producto");
         repositorio.eliminar(id);
         getLogger().info("Producto eliminado");
+        getLogger().info("\n\n\n");
     }
 }
